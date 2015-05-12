0b045e1773b9c7ef9b4488d2be29af0fb36a204e
yuqing123 <yuqingp@stanford.edu>
2015-05-12 13:54:44 -0700

    tutorial
    tutorial

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,8 +1,20 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
 @app.route("/")
 def index():
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
