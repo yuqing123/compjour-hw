4d0f678a72009bd467f41c9d5ef4f0a6fb5b71bb
yuqing123 <yuqingp@stanford.edu>
2015-06-06 22:59:59 -0700

    search script scrape
    search script scrape

--- search-script-scrape/68(wrong).py
+++
@@ -1,8 +0,0 @@
-import requests
-from lxml import html
-response = requests.get('https://www.accessdata.fda.gov/scripts/cder/ob/docs/tempah.cfm')
-doc = html.fromstring(response.text) 
-tables = doc.cssselect('table')[0]
-
-row = tables.cssselect('tr')
-print (len(row))
