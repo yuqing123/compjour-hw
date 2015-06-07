f76a73b61531186e382255c7ce186a3a31b63e33
yuqing123 <yuqingp@stanford.edu>
2015-06-06 22:59:24 -0700

    search script scrape
    search script scrape

--- search-script-scrape/19(html table).py
+++
@@ -1,7 +0,0 @@
-import requests
-from lxml import html
-response = requests.get('http://www.asias.faa.gov/pls/apex/f?p=100:93:0::NO:::')
-doc = html.fromstring(response.text) 
-tables = doc.cssselect('table')[1]
-first_row = tables.cssselect('tr')[1]
-print(first_row.text_content())
