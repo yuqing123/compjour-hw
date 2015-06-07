0f6e330bf7f6be48b5ca0de830a9a76b6786ec9e
yuqing123 <yuqingp@stanford.edu>
2015-06-06 22:59:47 -0700

    search script scrape
    search script scrape

--- search-script-scrape/57(wrong).py
+++
@@ -1,6 +0,0 @@
-from lxml import html
-import requests
-page = requests.get('http://www.regulations.gov/#!docketDetail;D=FDA-2015-N-0540')
-tree = html.fromstring(page.text)
-result = tree.xpath('//span[@id="commentsShow"]/span[@class="GCARQJCDBWD"]/span/text()')
-print (result)
